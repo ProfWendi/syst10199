61ff525cdf351f84520e8a137d9bbea425e69750
ProfWendi <jollymor@sheridancollege.ca>
2023-08-24 16:53:21 -0400

    changed how the url mapping worked

--- nodejs/routes2/app.js
+++ nodejs/routes2/app.js
@@ -5,26 +5,10 @@ const port = 3000,
   // fs module is for working with the file system
   fs = require("fs"); 
 
-// Create a function that accepts a URL pattern and 
-// returns an HTML file name in the /views directory.
-// Note that if you've been looking at req.url in the 
-// previous examples, you'll notice it always starts with "/"
-// so 'views${url}.html' will be views/whatever.html
-const getViewUrl = (url) => {                 
-    return `views${url}.html`;
-};
-
-// a map of the different HTML files we 
-// want to map to
-/*const routeMap = {                                      
-  "/": "views/index.html", // index page 
-  "/page2": "views/page2.html", // page 2
-};*/
-
 http.createServer((req, res) => {
     
     // get the file name for the requested URL
-    let viewUrl = getViewUrl(req.url);
+    let viewUrl = `views${req.url}`;
     // for debugging:
     console.log(viewUrl);
 
@@ -39,20 +23,21 @@ http.createServer((req, res) => {
     // readFile() returns any errors that occurred (error param of callback)
     fs.readFile(viewUrl, (error, data) => {
 
+        res.setHeader("Content-Type", "text/html");
+
         if (error) { // error finding/reading file
-            res.writeHead(404);  // 404 = NOT FOUND
+            res.statusCode = 404; // 404 = NOT FOUND
             res.write("<h1>File Not Found</h1>");
             console.log(error);  // for debugging
 
         } else {  // no errors, get the file
-            res.writeHead(200, {  // 200 OK, returning text and HTML
-                "Content-Type": "text/html"
-            });
+            res.statusCode = 200; // 200 = OK
             // data contains the file contents
             // write the file's contents to the response body
             res.write(data);
         }
         // all done either way, send the response
+        
         res.end();
     });
 }).listen(port);
