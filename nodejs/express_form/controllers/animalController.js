d95d2799c785440db28aa2d026ec837ec958c95a
ProfWendi <jollymor@sheridancollege.ca>
2023-08-27 16:16:27 -0400

    Create animalController.js

---
+++ nodejs/express_form/controllers/animalController.js
@@ -0,0 +1,12 @@
+const chineseYear = require("chinese-year");
+
+exports.getAnimal = (req, res) => {
+    // make sure the year input has an actual year in it
+    if (!req.body.year) {
+        req.body.year = new Date().getFullYear();
+    }
+    // get the animal for the year
+    const animal = chineseYear.getAnimal(req.body.year);
+    // add the animal to the query string
+    req.body.animal = animal;
+}
