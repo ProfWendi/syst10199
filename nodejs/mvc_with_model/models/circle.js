2102ef8179c22ebef00188327c1bd6a6c4d0dfe3
ProfWendi <jollymor@sheridancollege.ca>
2023-09-21 16:38:33 -0400

    Update circle.js

--- nodejs/mvc_with_model/models/circle.js
+++ nodejs/mvc_with_model/models/circle.js
@@ -1,4 +1,19 @@
-function Circle(radius) {
+class Circle {
+    constructor(radius=1) {
+        this.radius = radius;
+    }
+    get circumference() {
+        return 2 * Math.PI * this.radius;
+    }
+    get area() {
+        return Math.PI * this.radius ** 2;
+    }
+    toString = function() {
+        return `Circle: radius=${this.radius}`;
+    }
+}
+/* example that uses a constructor function, instead */
+/*function Circle(radius=1) {
     this.radius = radius;
     Object.defineProperty(this, "circumference", {
         get() {
@@ -14,9 +29,9 @@ function Circle(radius) {
         return `Circle: radius=${this.radius}`;
     }
 }
-
+*/
 module.exports.getDefaultCircle = () => {
-    return new Circle(1);
+    return new Circle();
 }
 
 module.exports.getCircle = (radius) => {
